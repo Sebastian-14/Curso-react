bb5186bfd54402ccca0aad1ccb3027a347315186
Sebastian <sebas14121999@gmail.com>
2019-04-09 23:27:30 -0500

    React

---
+++ build/precache-manifest.7d706e0551f85827efdcb1a12531323f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "454d3da14c55affbd4edeb6cbd3fec0b",
+    "url": "/Curso-react/static/media/phoenix.454d3da1.png"
+  },
+  {
+    "revision": "95340977c8857cc24959",
+    "url": "/Curso-react/static/js/runtime~main.fa0250fa.js"
+  },
+  {
+    "revision": "6ce8c37a2e0d79bdb1ed",
+    "url": "/Curso-react/static/js/main.e49987ec.chunk.js"
+  },
+  {
+    "revision": "8e5d5aa49ff12202930c",
+    "url": "/Curso-react/static/js/2.8bd31de7.chunk.js"
+  },
+  {
+    "revision": "6ce8c37a2e0d79bdb1ed",
+    "url": "/Curso-react/static/css/main.72e7840d.chunk.css"
+  },
+  {
+    "revision": "8e5d5aa49ff12202930c",
+    "url": "/Curso-react/static/css/2.5ade3184.chunk.css"
+  },
+  {
+    "revision": "b69eb5dcc89fb9e62aad76d76bfbb55e",
+    "url": "/Curso-react/index.html"
+  }
+];
